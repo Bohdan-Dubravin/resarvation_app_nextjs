d9a3142cb356c7639c87cfa9fad5a06f8cc23ce4
Bohdan-Dubravin <dubravin111@gmail.com>
2023-03-23 09:37:24 +0200

    add links

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,21 @@
-import Image from 'next/image';
+'use client';
 import { Inter } from '@next/font/google';
 import styles from './page.module.css';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
+  const router = useRouter();
+  const [location, setLocation] = useState<string>();
+
   return (
     <main className="bg-gray-100 min-h-screen w-screen">
       <main className="max-w-screen-2xl m-auto bg-white">
         {/* NAVBAR */}
-        <nav className="bg-white p-2 flex justify-between">
-          <a href="" className="font-bold text-gray-700 text-2xl">
-            {' '}
-            OpenTable{' '}
-          </a>
-          <div>
-            <div className="flex">
-              <button className="bg-blue-400 text-white border p-1 px-4 rounded mr-3">
-                Sign in
-              </button>
-              <button className="border p-1 px-4 rounded">Sign up</button>
-            </div>
-          </div>
-        </nav>
+
         {/* NAVBAR */}
         <main>
           {/* HEADER */}
@@ -37,8 +30,12 @@ export default function Home() {
                   className="rounded  mr-3 p-2 w-[450px]"
                   type="text"
                   placeholder="State, city or town"
+                  onChange={(e) => setLocation(e.target.value)}
                 />
-                <button className="rounded bg-red-600 px-9 py-2 text-white">
+                <button
+                  onClick={() => {}}
+                  className="rounded bg-red-600 px-9 py-2 text-white"
+                >
                   Let's go
                 </button>
               </div>
